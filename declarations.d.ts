d525948c7c26efa50257fc1a2fff37d312827745
Alexander Dang <alexdang1993374@gmail.com>
2023-06-01 16:41:51 -0700

    feat: popular jobs

--- declarations.d.ts
+++ declarations.d.ts
@@ -7,3 +7,8 @@ declare module "*.jpg" {
   const value: any;
   export default value;
 }
+
+declare module "@env" {
+  export const RAPID_API_KEY: string;
+  // add other environment variables here
+}
