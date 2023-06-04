3d7ab43b3ce3019b3483bde53c7f0db4a08829f1
Alexander Dang <alexdang1993374@gmail.com>
2023-06-04 10:41:03 -0700

    fix: ui

--- components/common/cards/nearby/NearbyJobCard.tsx
+++ components/common/cards/nearby/NearbyJobCard.tsx
@@ -1,19 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { checkImageURL } from "../../../../utils";
 
 import styles from "./nearbyjobcard.style";
 import { IItem } from "../../../../types";
 
-interface INearbyJobCard {
+interface IPopularJobCard {
   job: IItem;
   handleNavigate: VoidFunction;
 }
 
-const NearbyJobCard = ({ job, handleNavigate }: INearbyJobCard) => {
+const NearbyJobCard = ({ job, handleNavigate }: IPopularJobCard) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity
+      // @ts-ignore
+      style={styles.container}
+      onPress={handleNavigate}
+    >
+      <TouchableOpacity
+        // @ts-ignore
+        style={styles.logoContainer}
+      >
+        <Image
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          // @ts-ignore
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text
+          // @ts-ignore
+          style={styles.jobName}
+          numberOfLines={1}
+        >
+          {job.job_title}
+        </Text>
+
+        <Text style={styles.jobType}>{job.employer_name}</Text>
+
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ export interface IItem {
   employer_name: string;
   job_title: string;
   job_country: string;
+  job_employment_type: string;
 }
 
 export type TJobType = "Full-time" | "Part-time" | "Contractor";
